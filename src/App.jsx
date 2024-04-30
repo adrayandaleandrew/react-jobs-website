5ab2a23aa309824a68857c3c919ff1f01ae792ff
Dale Adrayan <adrayandaleandrew@gmail.com>
2024-04-30 22:47:43 +0800

    removed all imported  components in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
-import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider, Router} from 'react-router-dom';
 import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route index element={<h1>My App</h1>} />)
+  createRoutesFromElements(
+  <Route path='/' element={<MainLayout />}>
+  <Route index element={<HomePage />} />
+  </Route>
+  )
 );
 
 const App = () => {
   return (
-   <>
-    <Navbar />
-    <Hero />
-    <HomeCards />
-    <JobListings />
-    <ViewAllJobs />
-   </>
+   <RouterProvider router={router} />
   );
 };
 
