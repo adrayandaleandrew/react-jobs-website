b8c6a52a84abe974c52008404e9a20b8ad5b05d6
Dale Adrayan <adrayandaleandrew@gmail.com>
2024-05-01 21:10:30 +0800

    add changes on Spinner, JobListing & Spinner

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import JobListing from './JobListing';
-import jobs from '../jobs.json';
+import { useState, useEffect } from "react";
+import JobListing from "./JobListing";
+import Spinner from "./Spinner";
 
-const JobListings = () => {
-   
-const recentJobs = jobs.slice(0, 3);
-    return (
+const JobListings = ({ isHome = false }) => {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const apiUrl = isHome ? 'http://localhost:8000/jobs?_limit=3' : 'http://localhost:8000/jobs';
+      try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchJobs();
+  }, []);
+  return (
     // <!-- Browse Jobs -->
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          Browse Jobs
+          {isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {recentJobs.map((job) => (
-                <JobListing key={job.id} job={ job }/>
-            ))};
-          
-        </div>
+        
+          {loading ? (
+            <Spinner loading={loading} />
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {jobs.map((job) => (
+                <JobListing key={job.id} job={job} />
+              ))}
+              ;
+              </div>
+          )}
+          ;
+        
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default JobListings
+export default JobListings;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 import logo from '../assets/images/logo.png';
 
 const Navbar = () => {
+
+    const linkClass =  ({isActive})=> isActive ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2' : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2';
+    
     return (
         <nav className="bg-indigo-700 border-b border-indigo-500">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="flex h-20 items-center justify-between">
                     <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
 
-                        <Link className="flex flex-shrink-0 items-center mr-4" to="/">
+                        <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
                             <img className="h-10 w-auto" src={ logo } alt="React Jobs"/>
                             <span className="hidden md:block text-white text-2xl font-bold ml-2">React Jobs</span>
-                        </Link>
+                        </NavLink>
                         <div className="md:ml-auto">
                             <div className="flex space-x-2">
-                                <Link to="/" className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">Home</Link>
-                                <Link to="/jobs" className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">Jobs</Link>
-                                <Link to="/add-job" className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">Add Job</Link>
+                                <NavLink to="/" className={linkClass}>Home</NavLink>
+                                <NavLink to="/jobs" className={linkClass}>Jobs</NavLink>
+                                <NavLink to="/add-job" className={linkClass}>Add Job</NavLink>
                             </div>
                         </div>
                     </div>
