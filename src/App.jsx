4e98f2110aa357fce79804090d9ccf787f9518b0
Dale Adrayan <adrayandaleandrew@gmail.com>
2024-05-01 21:19:28 +0800

    modified the json docu, main App and JobsPage

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
     <Route index element={<HomePage />} />
     <Route path='/jobs' element={<JobsPage />} />
+    <Route path='*' element={<NotFoundPage />} />
   </Route>  
   )
 );
