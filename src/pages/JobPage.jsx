c785718f7edf63aa85a78fd805edb6a383525d7c
Dale Adrayan <adrayandaleandrew@gmail.com>
2024-05-07 18:17:49 +0800

    import toast, made change on JobPage func, add onDeleteClick func,add func on buttons,

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 // import { useState, useEffect } from 'react';
-import { useParams, useLoaderData, Link } from "react-router-dom";
+import { useParams, useLoaderData, useNavigate, Link } from "react-router-dom";
 import { FaArrowLeft } from "react-icons/fa"; 
 import {FaMapMarker} from 'react-icons/fa';
+import {toast} from 'react-toastify';
 
-const JobPage = () => {
+const JobPage = ( {deleteJob} ) => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const job = useLoaderData();
+
+  const onDeleteClick = (jobId) => {
+    const confirm = window.confirm('Are you sure you want to delete this listing? ')
+
+    if(!confirm) return;
+
+    deleteJob(jobId);
+
+    toast.success('Job deleted successfully')
+    navigate('/jobs')
+  }
 
   return (
     <>
@@ -83,7 +96,7 @@ const JobPage = () => {
                 >
                   Edit Job
                 </Link>
-                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                <button onClick={ () => onDeleteClick(job.id) } className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
                   Delete Job
                 </button>
               </div>
