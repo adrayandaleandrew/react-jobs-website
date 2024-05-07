2953cacf98eac1fb0da0b6c2371711cdfc61a32d
Dale Adrayan <adrayandaleandrew@gmail.com>
2024-05-07 19:22:40 +0800

    updated the button inside text and header

--- src/pages/EditJobPage.jsx
+++ src/pages/EditJobPage.jsx
@@ -49,7 +49,7 @@ const EditJobPage = ( {updateJobSubmit} ) => {
       <div className="container m-auto max-w-2xl py-24">
         <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
           <form onSubmit={submitForm}>
-            <h2 className="text-3xl text-center font-semibold mb-6">Add Job</h2>
+            <h2 className="text-3xl text-center font-semibold mb-6">Update Job</h2>
 
             <div className="mb-4">
               <label
@@ -230,7 +230,7 @@ const EditJobPage = ( {updateJobSubmit} ) => {
                 className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
                 type="submit"
               >
-                Add Job
+                Update Job
               </button>
             </div>
           </form>
